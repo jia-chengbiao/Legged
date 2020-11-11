01769483d02e560e5086176c09bab9f986d76f42
jia-chengbiao <jia-chengbiao@users.noreply.github.com>
2020-11-11 12:25:28 +0800

    第一次上传

---
+++ Core/Inc/main.h
@@ -0,0 +1,137 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* 板级固件包头文件引用 */
+#include <stdio.h>
+#include <string.h>
+#include "bsp_led.h"
+#include "bsp_IIC.h"
+#include "bsp_pca9685.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+extern const float *Clock_t(void);
+extern char ListDataBuffer[1000];
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define IIC_SCL_Pin GPIO_PIN_0
+#define IIC_SCL_GPIO_Port GPIOB
+#define IIC_SDA_Pin GPIO_PIN_1
+#define IIC_SDA_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* 定义 */
+#define pi 3.14159265358979        //圆周率
+
+/** 弧度制与角度制转角度度制
+ *  angle to rad = angle *  Angle_To_Rad
+ *  rad to angle = rad * Rad_To_Angle
+ **/
+#define Angle_To_Rad 0.0174533f    //角度转弧度制
+#define Rad_To_Angle 57.2957795f   //弧度转角度制
+
+/** IO口输入输出宏
+*/
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//IO地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+ 
+//IO口操作，只对单一的IO操作
+//确保n值小�?16
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
